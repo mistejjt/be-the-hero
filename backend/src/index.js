c0a092db30c6ac0643af03e7fda9387a5c2ae8df
mistejjt <mistejjt@gmail.com>
2020-03-28 00:28:42 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+//iniciar com node index.js no terminal
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
